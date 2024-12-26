359de7977a152d6ad78dd38c461dd8387a1a9b2f
fiyosa <fiyosa97@gmail.com>
2024-12-26 11:28:48 +0700

    edit: nonels

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -21,7 +21,7 @@ return {
         },
       })
 
-      vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
     end,
   },
 }
