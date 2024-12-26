b2af77d468b03e18060364beaf638113522118f7
fiyosa <fiyosa97@gmail.com>
2024-12-26 10:28:55 +0700

    add: nvimtree

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right", -- Explorer di sebelah kanan
+      width = 30,         -- Lebar jendela explorer (opsional)
+    },
+  },
+}
