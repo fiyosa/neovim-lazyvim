212386847f391b73c9c3116c77dcf2f2d62a6427
fiyosa <fiyosa97@gmail.com>
2024-12-26 11:57:59 +0700

    edit: nonels

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,9 +1,18 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    window = {
-      position = "right", -- Explorer di sebelah kanan
-      width = 30,         -- Lebar jendela explorer (opsional)
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right", -- Explorer di sebelah kanan
+        width = 30,         -- Lebar jendela explorer (opsional)
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,            -- Default: hidden items disembunyikan
+          hide_dotfiles = false,     -- Tidak menyembunyikan file/direktori yang dimulai dengan "."
+          hide_gitignored = false,   -- Tidak menyembunyikan file yang diabaikan oleh Git
+        },
+      },
     },
-  },
+  }
 }
