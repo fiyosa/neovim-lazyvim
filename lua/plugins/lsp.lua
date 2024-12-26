58990f80a217569d2ebb6acef2bc0ed5cbedd4aa
fiyosa <fiyosa97@gmail.com>
2024-12-26 18:18:57 +0700

    feat

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,114 +1,131 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"cssls",
-					"html",
-					"emmet_ls",
-					"ts_ls",
-					"gopls",
-					"svelte",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
-			lsp_capabilities.textDocument.completion.completionItem.snippetSupport = true
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({})
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "cssls",
+          "html",
+          "emmet_ls",
+          "ts_ls",
+          "gopls",
+          "svelte",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+      lsp_capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-			lspconfig.emmet_ls.setup({
-				capabilities = lsp_capabilities,
-				filetypes = {
-					"css",
-					"html",
-					"javascript",
-					"javascriptreact",
-					"sass",
-					"scss",
-					"svelte",
-					"typescriptreact",
-				},
-				init_options = {
-					html = {
-						options = {
-							-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-							["bem.enabled"] = true,
-						},
-					},
-				},
-			})
+      lspconfig.emmet_ls.setup({
+        capabilities = lsp_capabilities,
+        filetypes = {
+          "css",
+          "html",
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "vue",
+          "sass",
+          "scss",
+          "svelte",
+        },
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      })
 
-			lspconfig.html.setup({
-				capabilities = lsp_capabilities,
+      lspconfig.html.setup({
+        capabilities = lsp_capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = lsp_capabilities,
+      })
+
+      -- lsp lua
+      lspconfig.lua_ls.setup({
+        capabilities = lsp_capabilities,
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = true
+        end,
+      })
+      -- lsp javascript
+      lspconfig.ts_ls.setup({
+        capabilities = lsp_capabilities,
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = true
+        end,
+      })
+      -- lsp vue
+      lspconfig.volar.setup({
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+        on_attach = function(client, bufnr)
+          -- Aktifkan format otomatis hanya dari Volar
+          if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ async = false })
+              end,
             })
-			lspconfig.cssls.setup({
-				capabilities = lsp_capabilities,
-            })
+          end
+        end,
+      })
+      -- lsp svelte
+      lspconfig.svelte.setup({
+        capabilities = lsp_capabilities,
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = true
+        end,
+      })
+      -- lsp go
+      lspconfig.gopls.setup({
+        capabilities = lsp_capabilities,
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = true
+        end,
+      })
 
-			-- lsp lua
-			lspconfig.lua_ls.setup({
-				capabilities = lsp_capabilities,
-				on_attach = function(client)
-					client.server_capabilities.documentFormattingProvider = true
-				end,
-			})
-			-- lsp javascript
-			lspconfig.ts_ls.setup({
-				capabilities = lsp_capabilities,
-				on_attach = function(client)
-					client.server_capabilities.documentFormattingProvider = true
-				end,
-			})
-			-- lsp svelte
-			lspconfig.svelte.setup({
-				capabilities = lsp_capabilities,
-				on_attach = function(client)
-					client.server_capabilities.documentFormattingProvider = true
-				end,
-			})
-			-- lsp go
-			lspconfig.gopls.setup({
-				capabilities = lsp_capabilities,
-				on_attach = function(client)
-					client.server_capabilities.documentFormattingProvider = true
-				end,
-			})
+      local opts = { noremap = true, silent = true }
 
-			local opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      -- code actions
+      vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 
-            -- code actions
-            vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+      vim.keymap.set("n", "<C-p>", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "<A-p>", vim.diagnostic.goto_next, opts)
+      vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
 
-			vim.keymap.set("n", "<C-p>", vim.diagnostic.goto_prev, opts)
-			vim.keymap.set("n", "<A-p>", vim.diagnostic.goto_next, opts)
-			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-
-			vim.keymap.set("n", "<C-K>", vim.lsp.buf.signature_help, opts)
-			vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-			vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-			vim.keymap.set("n", "<leader>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, opts)
-			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-			vim.keymap.set("n", "<leader>E", vim.diagnostic.open_float, opts)
-			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
-		end,
-	},
+      vim.keymap.set("n", "<C-K>", vim.lsp.buf.signature_help, opts)
+      vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+      vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+      vim.keymap.set("n", "<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, opts)
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "<leader>E", vim.diagnostic.open_float, opts)
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+    end,
+  },
 }
--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -7,13 +7,24 @@ return {
       -- setting formater yang akan dipakai
 
       null_ls.setup({
+        debug = true,
         sources = {
           -- stylua
           null_ls.builtins.formatting.stylua,
 
           -- prettier
           null_ls.builtins.formatting.prettier.with({
-            filetypes = { "css", "html", "javascript", "typescript", "json", "yaml" },
+            filetypes = {
+              "css",
+              "html",
+              "javascript",
+              "typescript",
+              "javascriptreact",
+              "typescriptreact",
+              "vue",
+              "json",
+              "yaml"
+            },
             dynamic_command = function()
               return "prettier"
             end,
@@ -22,6 +33,9 @@ return {
       })
 
       vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+      -- vim.keymap.set("n", "<leader>cf", function()
+      --   vim.lsp.buf.format({ timeout_ms = 5000 }) -- timeout 5 detik
+      -- end, {})
     end,
   },
 }
